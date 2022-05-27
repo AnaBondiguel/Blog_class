4eba5cd01e94cd06b9282a0480acb971bd0a5921
AnaBondiguel <michaelyangyang@hotmail.com>
2022-05-27 23:26:47 +1000

    initial commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,59 @@
+class ArticlesController < ApplicationController
+    def index
+        @time = Time.now
+        @articles = Article.all
+
+      #  console
+    end
+
+    def show
+        @article = Article.find(params[:id])
+      
+     #   console
+    end 
+
+    def new
+        @article = Article.new
+
+      #  console
+    end
+
+    def create
+        @article = Article.new(article_params)
+        if @article.save
+            redirect_to articles_path
+       else
+        render :new
+           
+       end
+    end
+
+
+    def edit
+        @article = Article.find(params[:id])
+
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update (article_params)
+            redirect_to articles_path
+       else
+        render :edit
+           
+       end
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+
+        @article.destroy
+
+        redirect_to home_path
+    end
+
+    private
+    def article_params
+        params.require(:article).permit(:title, :body, :importance)
+    end
+end
